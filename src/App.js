9176c121e0bd98d4bd75dc3b517988d61033e01b
Svajunas Kaseta <svajunas@aventusgroup.com>
2019-03-11 13:40:44 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, {Component} from 'react';
+import classes from './App.css';
+
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 1, name: "Max", age: 28},
+      {id: 2, name: "AAAA", age: 55},
+      {id: 3, name: "LLL", age: 12},
+  ],
+    showPerson: false
+  };
+
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState( {persons: persons})
+  };
+
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  };
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow});
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+
+    };
+
+    let persons = null;
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)}/>
+          })}
+        </div>
+      );
+      style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
+    }
+
+
+
+    return (
+      <div className={classes.App}>
+        <h1>ssss</h1>
+        <button
+          style={style}
+          onClick={this.togglePersonHandler}>Switch name
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
